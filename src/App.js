59097d61659f5771dcb23d87c9ab9185940a6178
Yosra-Hamdi <hamdiyosra010@gmail.com>
2024-12-27 14:40:52 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+function App() {
+  return (
+   <div>
+     <h1 className="title">
+      New branch
+     </h1>
+     <h2> yosr</h2>
+   </div>
+  /*<>
+      <h1>
+       bonjour
+      </h1>
+      <h2> hello world</h2>
+  </>*/
+
+
+  
+  );
+}
+
+export default App;
